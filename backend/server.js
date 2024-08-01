c4c136d83298cd7a2765feb5315eca6cae90e4d1
safi100 <safinafi12@gmail.com>
2024-08-01 19:54:21 +0300

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(morgan('dev'));
 
+// conntect the database
+const DB_URL = process.env.DATABASE
+mongoose.connect(DB_URL)
+.then(() => console.log('Database connected...'))
+.catch(err => console.log(err));
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);
 })
