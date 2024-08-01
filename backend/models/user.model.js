64807418fb86d3f023868f38fa2979876042e7e5
safi100 <safinafi12@gmail.com>
2024-08-01 19:55:05 +0300

    Create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    role: {
+        type: String,
+        enum: ['student', 'librarian'],
+        default: 'student'
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
