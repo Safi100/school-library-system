e87aedf241a1a7010849b260d55b52492d7243b9
safi100 <safinafi12@gmail.com>
2024-08-03 14:17:36 +0300

    Setup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+
+// pages
+const RegisterPage = lazy(() => import('./pages/auth/Register'))
+const LoginPage = lazy(() => import('./pages/auth/Login'));
+const VerifyEmailPage = lazy(() => import('./pages/auth/VerifyEmail'))
+const HomePage = lazy(() => import('./pages/home/Home'));
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
