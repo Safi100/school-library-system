05d83ca038b45eff508e4dc941c9964cac68751d
safi100 <safinafi12@gmail.com>
2024-08-04 19:32:48 +0300

    Working on navbar

---
+++ frontend/src/components/NavigationBar.jsx
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const NavigationBar = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-dark navbar-dark">
+      <Container>
+        <Navbar.Brand href="/">Library</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+          <NavDropdown title={`D`} id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
