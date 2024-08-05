d100abc8963fa7db1cfcbd1242f54b88a7142e40
safi100 <safinafi12@gmail.com>
2024-08-05 20:17:05 +0300

    Create category model

---
+++ backend/models/category.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Category name is required'],
+        unique: true
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Category', categorySchema)
