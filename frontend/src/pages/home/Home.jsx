a1c95600a3b5e8d91188b40a7fc0a4e29235bb53
safi100 <safinafi12@gmail.com>
2024-08-06 22:25:08 +0300

    Display categories on home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import Axios from 'axios'
 
 const Home = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    Axios.get('http://localhost:8000/api/category')
+     .then(res => setCategories(res.data))
+     .catch(error => console.error(error))
+  }, [])
+
   return (
-    <div className=''>Home</div>
+    <div className='wrapper py-4'>
+      <div className="row gap-4">
+        {categories.map(category => (
+          <a href={`/books/${category.name}`} key={category.id} className='card col col-3 mb-3'>
+            <div className='card-body'>
+              <h5 className='card-title'>{category.name}</h5>
+              <p className='card-text'>{category.description}</p>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
   )
 }
 
