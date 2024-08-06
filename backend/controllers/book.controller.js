ba49af6e26a7f8b650ab4f1c1b682e82c7dec6e5
safi100 <safinafi12@gmail.com>
2024-08-06 21:45:32 +0300

    Find books by category

--- backend/controllers/book.controller.js
+++ backend/controllers/book.controller.js
@@ -1,10 +1,13 @@
 const Book = require('../models/book.model')
+const Category = require('../models/category.model');
 const HandleError = require('../utils/HandleError');
 
 module.exports.getBooksByCategory = async (req, res, next) => {
     try{
-        const {category} = req.query;
-        const books = await Book.find({ categories: { $in: [category] } }).populate('categories');
+        const {category} = req.params;
+        const cat = await Category.findOne({ name: category }).select('_id');
+        if(!cat) throw new HandleError('Category not found', 404);
+        const books = await Book.find({ categories: { $in: [cat._id] } }).populate('categories');
         res.status(200).json(books);
     }catch(error){
         console.log(error);
