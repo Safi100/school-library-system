4fdb0cec58032b02c7f6c2331a93975b76234263
safi100 <safinafi12@gmail.com>
2024-08-08 02:00:49 +0300

    add caplitalize func

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,11 +1,13 @@
 const Category = require('../models/category.model')
 
+// Helper function to capitalize a string
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1)
 
 module.exports.newCategory = async (req, res, next) => {
     try{
         const categoryName = req.body.categoryName.trim();
         const category = new Category({
-            name: categoryName
+            name: capitalize(categoryName)
         });
         await category.save();
         res.status(201).json(category);
