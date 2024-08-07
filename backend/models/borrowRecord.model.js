74217e26c228e0b65a6215fa94f001ab70606b66
safi100 <safinafi12@gmail.com>
2024-08-07 14:55:28 +0300

    working on borrow model

---
+++ backend/models/borrowRecord.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const BorrowRecordSchema = new Schema({
+    bookId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Book',
+        required: [true, 'Book ID is required']
+    },
+    borrower: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Borrower ID is required']
+    },
+    borrowDate: {
+        type: Date,
+        default: new Date()
+    },
+    returnDate: {
+        type: Date,
+        default: null
+    }
+})
+
+module.exports = mongoose.model('BorrowRecord', BorrowRecordSchema)
