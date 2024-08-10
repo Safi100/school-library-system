b4e8080a4bc63871725157006770a21f219dc695
safi100 <safinafi12@gmail.com>
2024-08-10 17:04:42 +0300

    Create api for display borrowed books

--- backend/controllers/borrow.controller.js
+++ backend/controllers/borrow.controller.js
@@ -1,5 +1,6 @@
 const Book = require('../models/book.model');
 const Student = require('../models/student.model');
+const BorrowBooks = require('../models/borrowRecord.model');
 const HandleError = require('../utils/handleError');
 
 module.exports.lendBook = async (req, res, next) => {
@@ -16,8 +17,13 @@ module.exports.lendBook = async (req, res, next) => {
         if(book.borrowed_by) throw new HandleError(`Book already borrowed by student: ${book.borrowed_by.name}`, 400);
         book.borrowed_by = student._id;
         student.borrowed_books.push(book._id);
+        const newBorrowRecord = new BorrowBooks({
+            bookId: book._id, 
+            borrower: student._id
+        });
         await book.save();
         await student.save();
+        await newBorrowRecord.save();
         res.status(200).send({message: "Book lent successfully", bookID: book._id});
     }catch(error){
         console.log(error);
@@ -30,6 +36,29 @@ module.exports.students_books = async (req, res, next) => {
         const students = await Student.find().select('name');
         const books = await Book.find({borrowed_by: {$eq: null}}).select('title');
         res.status(200).json({students, books});
+    }catch(error){
+        console.log(error);
+        next(error);   
+    }
+}
+
+module.exports.borrowed_books = async (req, res, next) => {
+    try{
+        let borrowBooks = await BorrowBooks.find()
+        .populate({path: 'book', populate: 'categories' })
+        .populate({ path: 'borrower', select: ['name', 'email', 'phone_number'] });
+        // Add warning to each book is over one week
+        borrowBooks = borrowBooks.map(book => {
+            const borrowDate = new Date(book.borrowDate);
+            const currentDate = new Date();
+            const diffTime = Math.abs(currentDate - borrowDate);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+            return {
+                ...book._doc,
+                warning: diffDays > 7
+            };
+        });
+        res.status(200).json(borrowBooks);
     }catch(error){
         console.log(error);
         next(error);   
--- backend/routes/borrow.route.js
+++ backend/routes/borrow.route.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { lendBook, students_books } = require('../controllers/borrow.controller')
+const { lendBook, students_books, borrowed_books } = require('../controllers/borrow.controller')
 const router = express.Router({mergeParams: true})
 const {isLoggedIn} = require('../middleware')
 
 router.get('/students_and_books', isLoggedIn, students_books)
 router.post('/lend-book/:book_id/:student_id', isLoggedIn, lendBook)
+router.get('/borrowed-books', isLoggedIn, borrowed_books)
 
 
 module.exports = router
