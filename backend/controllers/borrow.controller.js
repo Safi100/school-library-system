38ade9122ac6132887bc50019a65d9b212b66182
safi100 <safinafi12@gmail.com>
2024-08-10 14:57:15 +0300

    handle null params and get all students and books

--- backend/controllers/borrow.controller.js
+++ backend/controllers/borrow.controller.js
@@ -5,6 +5,10 @@ const HandleError = require('../utils/handleError');
 module.exports.lendBook = async (req, res, next) => {
     try{
         const { book_id, student_id } = req.params;
+        // validate
+        if (book_id === 'null' || book_id === null || student_id === 'null' || student_id === null) {
+            throw new HandleError('Book ID and Student ID are required', 400);
+        }
         const book = await Book.findById(book_id).populate('borrowed_by');
         if(!book) throw new HandleError('Book not found', 404);
         const student = await Student.findById(student_id);
@@ -14,9 +18,20 @@ module.exports.lendBook = async (req, res, next) => {
         student.borrowed_books.push(book._id);
         await book.save();
         await student.save();
-        res.status(200).send("Book lent successfully");
+        res.status(200).send({message: "Book lent successfully", bookID: book._id});
     }catch(error){
         console.log(error);
         next(error);
+    }
+}
+
+module.exports.students_books = async (req, res, next) => {
+    try{
+        const students = await Student.find().select('name');
+        const books = await Book.find({borrowed_by: {$eq: null}}).select('title');
+        res.status(200).json({students, books});
+    }catch(error){
+        console.log(error);
+        next(error);   
     }
 }
--- backend/routes/borrow.route.js
+++ backend/routes/borrow.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { lendBook } = require('../controllers/borrow.controller')
+const { lendBook, students_books } = require('../controllers/borrow.controller')
 const router = express.Router({mergeParams: true})
 const {isLoggedIn} = require('../middleware')
 
+router.get('/students_and_books', isLoggedIn, students_books)
 router.post('/lend-book/:book_id/:student_id', isLoggedIn, lendBook)
 
 
