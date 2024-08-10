e243ceb2160eead94f8882ec9700cb0a53c0cb92
safi100 <safinafi12@gmail.com>
2024-08-10 17:04:07 +0300

    fix ref

--- backend/models/borrowRecord.model.js
+++ backend/models/borrowRecord.model.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const BorrowRecordSchema = new Schema({
-    bookId: {
+    book: {
         type: Schema.Types.ObjectId,
         ref: 'Book',
         required: [true, 'Book ID is required']
     },
     borrower: {
         type: Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'Student',
         required: [true, 'Borrower ID is required']
     },
     borrowDate: {
