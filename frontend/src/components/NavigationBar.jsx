b1b10d45186ad48acbdcc258e7fdc96aecb4d07b
safi100 <safinafi12@gmail.com>
2024-08-12 23:35:23 +0300

    Create logout function

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -2,8 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useNavigate } from 'react-router-dom';
+import Axios from 'axios';
 
 const NavigationBar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    Axios.post('http://localhost:8000/api/auth/logout')
+    .then(() => {
+      navigate('/login');
+    })
+    .catch(error => console.log(error));
+  }
+
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-dark navbar-dark">
       <Container>
@@ -24,7 +36,7 @@ const NavigationBar = () => {
           <NavDropdown title={`Profile`} id="collapsible-nav-dropdown">
               <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="">Logout</NavDropdown.Item>
+              <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
